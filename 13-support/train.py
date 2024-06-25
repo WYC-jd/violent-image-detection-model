3b2efdef3955c929cec82d30400057324c204183
WYC-jd <jd2022wyc@sjtu.edu.cn>
2024-06-25 21:29:13 +0800

    Add files via upload

--- 13-support/train.py
+++ 13-support/train.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
     multiprocessing.freeze_support()
 
     # 参数设置
-    gpu_id=[1]
+    # gpu_id=[1]
     lr = 3e-4
     batch_size = 128
     log_name = "resnet18_pretrain_test"
-    print("{} gpu:{}, batch size: {}, lr: {}".format(log_name, gpu_id, batch_size, lr))
+    print("{} cpu, batch size: {}, lr: {}".format(log_name, batch_size, lr))
 
     # 创建数据模块
     data_module = CustomDataModule(batch_size=batch_size)
@@ -34,8 +34,8 @@ if __name__ == '__main__':
     # 创建 Trainer 实例
     trainer = Trainer(
         max_epochs=40,
-        accelerator='gpu',  # 使用GPU
-        devices=gpu_id,   # 如果使用GPU，需要配置合适的设备ID
+        accelerator='cpu',  # 使用CPU
+        # devices=gpu_id,   # 如果使用GPU，需要配置合适的设备ID
         logger=logger,
         callbacks=[checkpoint_callback]
     )
--- 13-support/using.py
+++ 13-support/using.py
@@ -1,39 +1,53 @@
+import torch
+import os
 from classify import ViolenceClass
-
+from model import ViolenceClassifier
 
 if __name__ == "__main__":
 
-    # 实例化该接口类
+    # 实例化模型和接口
 
-    using_example = ViolenceClass()
-
-    # 加载checkpoint并返回模型
+    model = ViolenceClassifier(learning_rate=3e-4)
+    using_example = ViolenceClass() 
     
-    checkpoint_path = 'path/to/your/checkpoint.pth'
-    model, epoch, loss = using_example.load_checkpoint(checkpoint_path, device='gpu')
+    # checkpoint_path = 'path/to/your/checkpoint.pth'
+    checkpoint_path = 'C:/Users/wyc/Desktop/violent_test/train_logs/resnet18_pretrain_test/version_0/checkpoints/resnet18_pretrain_test-epoch=08-val_loss=0.05.ckpt'
+        
+    if not os.path.exists(checkpoint_path):
+        raise FileNotFoundError(f'No checkpoint found at {checkpoint_path}')
 
-    # 打印轮次，损失值
+    # 加载checkpoint
 
-    print(f'Model loaded. Epoch: {epoch}, Loss: {loss}')
+    checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))
+
+    # 加载模型
+    model.load_state_dict(checkpoint['state_dict'])
+    model.eval()  
 
     # 从指定路径批量加载图片,返回一个图片列表，图片格式为RGB
 
-    img_example_list = using_example.load_images_from_path('/path/to/your/image') //"C:/Users/wyc/Desktop/暴力图片检测模型/output/output"
+    # img_example_list = using_example.load_images_from_path('/path/to/your/image') 
+    img_example_list = using_example.load_images_from_path('C:/Users/wyc/Desktop/violent_test/violence_224/test')
 
     # 转换图片列表到tensor格式
 
-    imgs_example = using_example.image_to_tensor(img_example_list)
+    imgs_example = using_example.images_to_tensor(img_example_list)
 
-    # 调用模型分类
+    # 使用模型进行推理
+    with torch.no_grad():
+        output_list = [model(img) for img in imgs_example]
 
-    predicted_output = using_example.classify(imgs_example)
+    # 调用模型获得真实和预测标签
+    label_true = using_example.label_true("test/")
+    label_predict = using_example.classify(output_list)
 
     # 计算正确率并打印
 
-    examples_accurancy = using_example.accuracy_score(using_example.label, predicted_output)
+    examples_accurancy = using_example.accuracy_score(label_true, label_predict)
     print(examples_accurancy)
 
     # 获得预测列表并打印
-    examples_pred = using_example.classify(imgs_example)
-    print(examples_pred)
+    print(label_predict)
+
+
 
